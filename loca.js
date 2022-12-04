cb41ea50412bdf8198e317b5d6fc09bf333aaca5
jmouad25 <119816801+jmouad25@users.noreply.github.com>
2022-12-04 23:32:50 +0100

    Add files via upload

---
+++ loca.js
@@ -0,0 +1,532 @@
+var loca = 
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 3500,
+        "Superficie": 48,
+        "Nombre de pieces": 2,
+        "Etage ": 4,
+        "Telephone": "0684851514"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.858864310995273,
+          33.99623844855088
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 4800,
+        "Superficie": 66,
+        "Nombre de pieces": 2,
+        "Etage": 4,
+        "Telephone": "0629848613"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.857598443080974,
+          33.995980110058284
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 6500,
+        "Superficie": 78,
+        "Nombre de pieces": 4,
+        "Etage": 2,
+        "Telephone": "0623217845"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.85518355659957,
+          33.99659366269593
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "prix": 12000,
+        "superficie": "95m²",
+        "nombre de pieces": 3,
+        "Etage": "2 éme étage"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.855193294044824,
+          33.99616579092921
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 7600,
+        "Superficie": 86,
+        "Nombre de pieces": 4,
+        "Etage": 2,
+        "Telephone": "0618498415"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.857756268536889,
+          33.995126850687555
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 7400,
+        "Superficie": 82,
+        "Nombre de pieces": 3,
+        "Etage": 2,
+        "Telephone": "062321786"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.855209469599629,
+          33.994917875935954
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 6500,
+        "Superficie": 95,
+        "Nombre de pieces": 5,
+        "Etage": 3,
+        "Telephone": "0715136951"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.855645313542226,
+          33.995052838855415
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 3500,
+        "Superficie": 50,
+        "Nombre de pieces": 2,
+        "Etage": 4,
+        "Telephone": "069849152"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.845993682386677,
+          34.00004742808903
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 4800,
+        "Superficie": 68,
+        "Nombre de pieces": 4,
+        "Etage": 4,
+        "Telephone": "069848123"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.847149746458172,
+          34.00243203489633
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 6250,
+        "Superficie": 52,
+        "Nombre de pieces": 3,
+        "Etage": 4,
+        "Telephone": "0698451615"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.84595239438363,
+          33.99856414606488
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 4200,
+        "Superficie": 65,
+        "Nombre de pieces": 4,
+        "Etage": 2,
+        "Telephone": "0678951512"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.847927336165242,
+          34.00104577584494
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 6000,
+        "Superficie": 85,
+        "Nombre de pieces": 4,
+        "Etage": 3,
+        "Telephone": "079515151"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.849565093600489,
+          34.00159914100077
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 6200,
+        "Superficie": 86,
+        "Nombre de pieces": 4,
+        "Etage": 3,
+        "Telephone": "069485151"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.849480814549793,
+          33.99739434849218
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 4400,
+        "Superficie": 67,
+        "Nombre de pieces": 3,
+        "Etage": 2,
+        "Telephone": "0698451618"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.850322181045925,
+          33.99823233566211
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 4200,
+        "Superficie": 84,
+        "Nombre de pieces": 4,
+        "Etage": 3,
+        "Telephone": "0698618516"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.847126117711014,
+          33.99814338769568
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 4000,
+        "Superficie": 85,
+        "Nombre de pieces": 4,
+        "Etage": 4,
+        "Telephone": "0698165171"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.847559018523867,
+          33.99593740412101
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 4200,
+        "Superficie": 59,
+        "Nombre de pieces": 5,
+        "Etage": 2,
+        "Telephone": "0629484618"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.849007011788302,
+          33.995686450846975
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 7000,
+        "Superficie": 85,
+        "Nombre de pieces": 3,
+        "Etage": 2,
+        "Telephone": "068451155"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.847134403214284,
+          33.99507261436986
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 6300,
+        "Superficie": 74,
+        "Nombre de pieces": 3,
+        "Etage": 2,
+        "Telephone": "068468418"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.848614119233673,
+          33.99433505125678
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 5500,
+        "Superficie": 78,
+        "Nombre de pieces": 3,
+        "Etage": 4,
+        "Telephone": "0615516515"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.847907194325785,
+          34.003301755627675
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 3900,
+        "Superficie": 75,
+        "Nombre de pieces": 2,
+        "Etage": 2,
+        "Telephone": "069898461"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.8483901289017695,
+          34.00200864403644
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 6500,
+        "Superficie": 95,
+        "Nombre de pieces": 4,
+        "Etage": 2,
+        "Telephone": "0699841552"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.846619368791124,
+          34.00280476133128
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 4200,
+        "Superficie": 76,
+        "Nombre de pieces": 3,
+        "Etage ": 3,
+        "Telephone": "0684615685"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.846314065323725,
+          34.0020868755463
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 4500,
+        "Superficie": 80,
+        "Nombre de pieces": 2,
+        "Etage ": 4,
+        "Telephone": "068826848"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.8527661731254454,
+          34.001484003662284
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 5000,
+        "Superficie": 80,
+        "Nombre de pieces": 3,
+        "Etage": 3,
+        "Telephone": "0606880000"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.853331020514702,
+          34.0020009252039
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 4300,
+        "Superficie": 65,
+        "Nombre de pieces": 4,
+        "Etage ": 3,
+        "Telephone": "068461558"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.84496896416303,
+          33.99697859764672
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 5200,
+        "Superficie": 52,
+        "Nombre de pieces": 3,
+        "Etage": 4,
+        "Telephone": "069848151"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.846355724250827,
+          33.99655688569223
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 5000,
+        "Superficie": 48,
+        "Nombre de pieces": 3,
+        "Etage": 2,
+        "Telephone": "0795511512"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.848726425984552,
+          33.99975735455246
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 5300,
+        "Superficie": 65,
+        "Nombre de pieces": 4,
+        "Etage": 4,
+        "Telephone": "06412116994"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.848783005804961,
+          33.99917273832247
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 5100,
+        "Superficie": 79,
+        "Nombre de pieces": 4,
+        "Etage ": 3,
+        "Telephone": "0698445615"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.845776461239723,
+          34.00100236809337
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Prix par mois": 4500,
+        "Superficie": 62,
+        "Nombre de pieces": 3,
+        "Etage": 3,
+        "Telephone": "0715165115"
+      },
+      "geometry": {
+        "coordinates": [
+          -6.8447294187277805,
+          33.998660278044156
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
